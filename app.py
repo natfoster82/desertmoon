113b3ccf55cf9e500df26585ee700cdb0280e27f
nat <nat.foster@gmail.com>
2017-04-23 14:08:46 -0600

    register views

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from extensions import socket
+from views import *
 # must import this
 import sock
 
@@ -7,6 +8,10 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
 
+    # register extensions
     socket.init_app(app)
+
+    # register views
+    app.register_blueprint(views_bp)
 
     return app
--- devserver.py
+++ devserver.py
@@ -7,4 +7,4 @@ from extensions import socket
 
 if __name__ == '__main__':
     application = create_app()
-    socket.run(application, host='0.0.0.0', port=6000)
+    socket.run(application, host='0.0.0.0', port=5000)
