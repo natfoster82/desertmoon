b8b3f57e067b34e5b284dff68d6a679e6bac48cb
nat <nat.foster@gmail.com>
2017-05-28 10:27:12 -0600

    simulation id

--- views.py
+++ views.py
@@ -8,5 +8,5 @@ views_bp = Blueprint('views', __name__, template_folder='templates')
 @views_bp.route('/')
 def index():
     handle = 'operator' + random_string(5, letters=False)
-    room = request.args.get('session', 'lobby')
+    room = request.args.get('simulation_id', 'lobby')
     return render_template('index.html', handle=handle, room=room)
